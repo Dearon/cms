d6b815e28c647158d0fe7064b3cbe01469e9e2f1
Dearon <dearon@gmail.com>
2014-06-08 15:02:54 +0200

    Changed the CSS library to Bootstrap

--- views/contact.html.php
+++ views/contact.html.php
@@ -1,26 +1,28 @@
 <h1>Contact</h1>
-<form action="/contact" method="post">
-    <? if (flash('error')) ?>
-        <p><?= html(flash('error')) ?></p>
-    <? endif; ?>
-    <? if (flash('success')) ?>
-        <p><?= html(flash('success')) ?></p>
-    <? endif; ?>
-    <p>
-        <label for="email">Your email address</label><br />
-        <input type="text" id="email" name="email" />
-    </p>
-    <? if (flash('error-email')) ?>
-        <p><?= html(flash('error-email')) ?></p>
-    <? endif; ?>
-    <p>
-        <label for="message">Your message</label><br />
-        <textarea id="message" name="message"></textarea>
-    </p>
-    <? if (flash('error-message')) ?>
-        <p><?= html(flash('error-message')) ?></p>
-    <? endif; ?>
-    <p>
-        <input type="submit" value="Send" />
-    </p>
+<form action="/contact" method="post" class="form-group">
+    <?php if (flash('error')): ?>
+        <div class="alert alert-danger"><?= html(flash('error')) ?></div>
+    <?php endif; ?>
+
+    <?php if (flash('success')): ?>
+        <div class="alert alert-success"><?= html(flash('success')) ?></div>
+    <?php endif; ?>
+
+    <div class="form-group">
+        <label for="email">Your email address</label>
+        <input type="email" class="form-control" id="email" name="email" value="<?= $email ?>" />
+        <?php if (flash('error-email')): ?>
+            <br /><div class="alert alert-danger"><?= html(flash('error-email')) ?></div>
+        <?php endif; ?>
+    </div>
+
+    <div class="form-group">
+        <label for="message">Your message</label>
+        <textarea class="form-control" id="message" name="message" rows="6"><?= $message ?></textarea>
+        <?php if (flash('error-message')): ?>
+            <br /><div class="alert alert-danger"><?= html(flash('error-message')) ?></div>
+        <?php endif; ?>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Send</button>
 </form>
--- views/layout.html.php
+++ views/layout.html.php
@@ -2,24 +2,32 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="/assets/css/build-full-no-icons.min.css">
+
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
     <div class="container">
-        <div class="col">
-            <div class="col width-2of10">
-                <div class="menu cell">
-                    <ul class="left nav">
-                        <li class="disabled"><?= $site_name ?></li>
-                        <?php foreach($nav as $item): ?>
-                            <li><a href="<?= $item['url'] ?>"><?= $item['title'] ?></a></li>
-                        <?php endforeach ?>
-                    </ul>
-                </div>
+        <div class="row">
+            <div class="col-sm-3" id="nav">
+                <h4><?= $site_name ?></h4>
+                <ul class="nav nav-pills nav-stacked">
+                    <?php foreach($nav as $item): ?>
+                        <li><a href="<?= $item['url'] ?>"><?= $item['title'] ?></a></li>
+                    <?php endforeach ?>
+                </ul>
             </div>
-            <div class="col width-fill content">
+            <div class="col-sm-9" id="article">
                 <?= content() ?>
             </div>
         </div>
